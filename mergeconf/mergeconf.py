6f1827a40f1928d8076bab8767cae69b3aa61b2d
Drew Leske <drew.leske@computecanada.ca>
2021-04-12 11:58:06 -0700

    Expose MergeConf class

--- mergeconf/mergeconf.py
+++ mergeconf/mergeconf.py
@@ -1,11 +1,5 @@
 # vi: set softtabstop=2 ts=2 sw=2 expandtab:
 # pylint: disable=W0621
-"""
-mergeconf - build a single configuration by merging multiple configuration
-sources with order of precedence, based on immediacy.  Currently: Default
-values are overridden by values read from configuration file which in turn are
-overridden by values read from environment variables.
-"""
 import os
 import logging
 import configparser
