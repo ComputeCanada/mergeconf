cbee072cceb479c954e7de9b29242fb1266b3195
Drew Leske <drew.leske@computecanada.ca>
2021-11-27 12:59:24 -0800

    Recursively merge environment variables

--- mergeconf/mergeconf.py
+++ mergeconf/mergeconf.py
@@ -57,11 +57,35 @@ class MergeConfSection():
     for name, section in self._sections.items():
       yield (name, section)
 
+  # get items for this section
+  # then for each subsection, check if any variables start with the section
+  # name and if so, strip the subsection name from them and send them to
+  # section._merge_env
+  # given envvars: list of variables with section prefix already stripped
+  #
+  def _merge_env(self, envvars):
+
+    # get any variable names for this section
+    for name, item in self._items.items():
+      if name in envvars:
+        item.value = envvars[name]
+
+    # see if any subsection has variables
+    for sectionname, section in self._sections.items():
+      prefix = sectionname + '_'
+      sectionvars = {
+        # TODO(3.9): replace `split(prefix, 1)[1]` with `removeprefix(prefix)`
+        x[0].split(prefix, 1)[1]: x[1]
+        for x in envvars.items() if x[0].startswith(prefix)
+      }
+      if sectionvars:
+        section._merge_env(sectionvars)
+
   def to_dict(self):
     # TODO(3.9): use union
     # return {
-    #   key: item.value for key, item in self._items.items() 
-    # } | { 
+    #   key: item.value for key, item in self._items.items()
+    # } | {
     #   name: section.to_dict() for name, section in self._sections.items()
     # }
     d = { key: item.value for key, item in self._items.items() }
@@ -143,8 +167,12 @@ class MergeConfSection():
     # check subsections
     for sectionname, section in self._sections.items():
       ss_missing = section.missing_mandatory()
+      # TODO: This is a pylint bug.  It applies to `sectionname`, but that
+      # should be locked in for each iteration of the enclosing for loop.
+      # see https://vald-phoenix.github.io/pylint-errors/plerr/errors/variables/W0640.html
+      # pylint: disable=cell-var-from-loop
       if ss_missing:
-        missing.extend(map(lambda i: f"{sectionnname}.{i}", ss_missing))
+        missing.extend(map(lambda i: f"{sectionname}.{i}", ss_missing))
 
     return missing or None
 
@@ -263,27 +291,14 @@ class MergeConf(MergeConfSection):
     prefix = self._codename.upper() + '_'
 
     # get all environment variables starting with that prefix into dict with
-    # prefix stripped from key
+    # key stripped of prefix and made lowercase
     envvars = {
-      # in Python 3.9, `split(prefix, 1)[1]` can be replaced with
-      # `removeprefix(prefix)`
-      x[0].split(prefix, 1)[1]: x[1]
+      # TODO(3.9): replace `split(prefix, 1)[1]` with `removeprefix(prefix)`
+      x[0].split(prefix, 1)[1].lower(): x[1]
       for x in os.environ.items() if x[0].startswith(prefix)
     }
 
-    # now map to sections and variables
-    # TODO: not recursive!
-    for sectionname, section in self._sections.items():
-      prefix = sectionname.upper() + '_'
-      for var in section._items:
-        envvarname = f"{prefix}{var.upper()}"
-        if envvarname in envvars:
-          # overwrite existing value
-          section._items[var].value = envvars[envvarname]
-    for name, item in self._items.items():
-      envvarname = name.upper()
-      if envvarname in envvars:
-        self._items[name].value = envvars[envvarname]
+    self._merge_env(envvars)
 
     return envvars
 
